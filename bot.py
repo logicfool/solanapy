acc66d38f07c90103cd1f50ea14b8789882e1654
Logic Fool <logicfool123121@gmail.com>
2021-10-07 19:22:53 +0530

    Private Key support added

--- bot.py
+++ bot.py
@@ -11,8 +11,11 @@ class solana_sniper():
     def __init__(self,rpc = "https://api.devnet.solana.com"):
         self.client = Client(rpc)
         self.config = load_config()['solana']
-        self.keypair = Keypair.from_seed(get_seed_from_mnemonic(self.config['mnemonic']))
-        self.account = account.Account(self.keypair)
+        if self.config.private_key:
+            self.keypair = Keypair.from_seed(get_seed_from_private_key(self.config['private_key']))
+        else:
+            self.keypair = Keypair.from_seed(get_seed_from_mnemonic(self.config['mnemonic']))
+        self.account = account.Account(self.keypair.secret_key[:32])
         self.initializer_balance = self.client.get_balance(self.account.public_key(), commitment=Recent).get('result').get('value')
 
 sol = solana_sniper()
--- utils.py
+++ utils.py
@@ -1,24 +1,57 @@
 import base58
 from bip_utils import Bip39SeedGenerator
+import base58
+import configparser
+import os,json
+class DotDict(dict):
+    """
+    a dictionary that supports dot notation 
+    as well as dictionary access notation 
+    usage: d = DotDict() or d = DotDict({'val1':'first'})
+    set attributes: d.val2 = 'second' or d['val2'] = 'second'
+    get attributes: d.val2 or d['val2']
+    """
+    __getattr__ = dict.__getitem__
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
 
-#load config from config.ini
+    def __init__(self, dct):
+        for key, value in dct.items():
+            if hasattr(value, 'keys'):
+                value = DotDict(value)
+            self[key] = value
+
+
 def load_config():
-    import configparser
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    return config
+    """Load Config from file"""
+    if os.path.exists('./config.ini'):
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        return DotDict(config)
+    else:
+        config = configparser.ConfigParser()
+        config['solana'] = {'mnemonic':'','private_key':''}
+        with open('config.ini', 'w') as configfile:
+            config.write(configfile)
+        print('Config created please edit the values to begin!')
+        exit()
+        # return DotDict(config)
 
-def get_key_pair(secret_key):
-    return 
-    byte_array = base58.b58decode(secret_key)
-    try:
-        json_string = "[" + ",".join(map(lambda b: str(b), byte_array)) + "]"
-    except:
-        raise Exception("Error while generating keypair is this a correct secret_key?")
-    print(json_string)
+def get_seed_from_private_key(private_key):
+    """Get the seed to use with keypair from private key
 
+    Args:
+        private_key (str): Private key
 
+    Returns:
+        Seed (str): Seed to use with keypair
+    """
+    key_seed = base58.b58decode(private_key)
+    return key_seed[:32]
 
 def get_seed_from_mnemonic(mnemonic):
     seed = Bip39SeedGenerator(mnemonic).Generate()
-    return seed[0:32]
+    return seed[:32]
+
+if __name__ == "__main__":
+    config = load_config()['solana']
